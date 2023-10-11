392930591870bcf991c6bf7bda8e05707f99b012
adam lamouri <adamlamouri.pro@gmail.com>
2023-10-12 00:44:01 +0200

    stockage darkmode

--- app/javascript/controllers/lightdark_controller.js
+++ app/javascript/controllers/lightdark_controller.js
@@ -2,11 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="lightdark"
 export default class extends Controller {
+  // Cette méthode est appelée lorsque le contrôleur est initialisé
   connect() {
+    this.loadTheme();
   }
 
+  // Basculer entre les thèmes et sauvegarder le choix de l'utilisateur
   transform() {
     document.body.classList.toggle('body-dm');
     document.body.classList.toggle('body-lm');
+
+    // Enregistre le choix actuel dans le localStorage
+    if (document.body.classList.contains('body-dm')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
+  // Charger le thème en fonction du choix précédent de l'utilisateur
+  loadTheme() {
+    const chosenTheme = localStorage.getItem('theme');
+
+    // Si l'utilisateur a précédemment choisi le dark mode
+    if (chosenTheme === 'dark') {
+      document.body.classList.add('body-dm');
+      document.body.classList.remove('body-lm');
+    }
+    // Si l'utilisateur a précédemment choisi le light mode ou n'a pas encore fait de choix
+    else {
+      document.body.classList.remove('body-dm');
+      document.body.classList.add('body-lm');
+    }
   }
 }
