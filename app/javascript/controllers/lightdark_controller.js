b0bb5aefdecd6cda0ddc4199716e90f93cbd0186
adam lamouri <adamlamouri.pro@gmail.com>
2023-10-12 00:36:43 +0200

    navbar full responsive

--- app/javascript/controllers/lightdark_controller.js
+++ app/javascript/controllers/lightdark_controller.js
@@ -3,11 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="lightdark"
 export default class extends Controller {
   connect() {
-    console.log('light and dark');
   }
 
   transform() {
-    console.log('dark');
     document.body.classList.toggle('body-dm');
     document.body.classList.toggle('body-lm');
   }
--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -11,9 +11,21 @@ export default class extends Controller {
     this.btnTargets.forEach((btn) => {
       btn.classList.toggle('d-none');
     });
-    this.menuTargets.forEach((div) => {
-      div.classList.toggle('responsive');
-    });
+
+    if (this.menuTarget.classList.contains('d-none')) {
+      // Si le menu est caché
+      this.menuTarget.classList.remove('d-none');
+      setTimeout(() => {
+        this.menuTarget.classList.add('show');
+      }, 10); // Une courte attente pour s'assurer que 'd-none' est bien supprimé avant d'ajouter 'show'.
+    } else {
+      // Si le menu est visible
+      this.menuTarget.classList.remove('show');
+      setTimeout(() => {
+        this.menuTarget.classList.add('d-none'); // Attend la fin de la transition (0.3s) avant d'ajouter 'd-none'.
+      },500);
+    }
   }
+
 
 }
